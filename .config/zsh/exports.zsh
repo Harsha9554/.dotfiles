771c35ec27974492b53a0b8a7f24ada57790021a
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-03-03 00:37:45 +0530

    dap-done

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -14,7 +14,11 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_DATA_DIRS="/usr/local/share"
 export XDG_CONFIG_DIRS="/etc/xdg"
 
-export NVIM_CONFIG_HOME="$XDG_CONFIG_HOME/nvim/"
+export NVIM_CONFIG_HOME="$XDG_CONFIG_HOME/nvim"
+export NVIM_INIT="$NVIM_CONFIG_HOME/init.lua"
+
+# py-virtual-envs
+export PY_VENV_HOME="$HOME/code/hole-venvs"
 
 # nvm
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
